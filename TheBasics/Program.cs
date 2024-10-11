6afbb436d1887bd9f64d80b1183d19709fec0eba
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-11 21:09:28 +0100

    Added init files for basics

---
+++ TheBasics/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+string test = "Test Name";
+
+Console.WriteLine($"Hello, World! {test}");
+
+
+// This line prevents the console from closing immediately
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey();
