c2d3e207a0a5eb54a2c4bdfe8cc48edf7f77acda
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-17 18:19:27 +0100

    Add Customer class

---
+++ OOPBasics/Customer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOPBasics
+{
+    internal class Customer
+    {
+        private readonly int _id;
+        private static int _nextId = 0;
+
+        public static int NumberOfCustomers = 0;
+
+        public int Id => _id;
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+        public Customer()
+        {
+            Name = "Unknown";
+            Address = "Unknown";
+            ContactNumber = "Unknown";
+
+            NumberOfCustomers++;
+            _id = _nextId++;
+        }
+
+        public Customer(string name) : this()
+        {
+            Name = name;
+        }
+
+        public Customer(string name, string address) : this()
+        {
+            Name = name;
+            Address = address;
+        }
+
+        public Customer(string name, string address, string contactNumber): this()
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine();
+        }
+
+        public void UpdateDetails(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+    }
+}
--- OOPBasics/Program.cs
+++ OOPBasics/Program.cs
@@ -6,7 +6,16 @@
         {
             Car audi = new Car("A3", "Audi");
             Car bmw = new Car("i7", "BMW", true);
+            Console.WriteLine();
 
+            Customer earl = new Customer("Earl");
+            Customer frank = new Customer("Frank", "Mainstreet 1", "5551234567");
+
+            Console.WriteLine($"{earl.Name} | {earl.Address} | {earl.ContactNumber}");
+
+            earl.UpdateDetails(earl.Name, "Newstreet 2", "5821234567");
+            Console.WriteLine($"{earl.Name} | {earl.Address} | {earl.ContactNumber}");
+            Console.WriteLine($"Earl ID: {earl.Id} | Frank ID: {frank.Id}");
             Console.ReadKey();
         }
     }
