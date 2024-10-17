3cb96aad06de212169bc44ddbe4c1304f5d51a39
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-17 16:20:41 +0100

    Add OOPBasics project with `Car` class

---
+++ OOPBasics/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace OOPBasics
+{
+    internal class Car
+    {
+        public string Model { get; }
+        public string Brand { get; } 
+        public bool IsLuxury { get; }
+
+        public string DisplayBrand => IsLuxury ? $"{Brand} - Luxury Edition" : Brand;
+
+        public Car(string model, string brand, bool isLuxury = false)
+        {
+            ErrorIfNullOrEmpty(model);
+            ErrorIfNullOrEmpty(brand);
+
+            Model = model;
+            Brand = brand;
+            IsLuxury = isLuxury;
+
+            Console.WriteLine($"An '{DisplayBrand}' of the model '{Model}' has been created.");
+        }
+
+        private static void ErrorIfNullOrEmpty(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
+    }
+}
---
+++ OOPBasics/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOPBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("A3", "Audi");
+            Car bmw = new Car("i7", "BMW", true);
+
+            Console.ReadKey();
+        }
+    }
+}
