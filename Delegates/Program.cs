a682f282b803216b4f5ac2bfdac82b2e76fe0698
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-10-23 11:38:15 +0100

    Add delegates/events project

---
+++ Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Delegates
+{
+    internal class Program
+    {
+        public delegate void Notify(string msg);
+
+        static void Main(string[] args)
+        {
+            // Delegates: provide a way to pass methods as parameters
+            Notify displayMsg = ShowMessage;
+
+            displayMsg("Hello!");
+
+            TemperatureMonitor monitor = new TemperatureMonitor();
+            TemperatureAlert alert = new TemperatureAlert();
+
+            monitor.OnTemperatureChange += alert.OnTemperatureChange;
+
+            // Code below is same as above subscription
+            //monitor.OnTemperatureChange += msg =>
+            //{
+            //    Console.WriteLine($"Alert: {msg}");
+            //};
+
+            monitor.Temperature = 20;  // No event
+            monitor.Temperature = 35;  // Runs event
+        }
+
+        static void ShowMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ Delegates/Temperature.cs
@@ -0,0 +1,40 @@
+﻿namespace Delegates
+{
+    public class TemperatureMonitor
+    {
+        // Equivalent to ...
+        //   public delegate void TemperatureChangeHandler(string msg);
+        //   public event TemperatureChangeHandler OnTemperatureChange;
+        public event Action<string>? OnTemperatureChange = null;
+
+        private int _temperature;
+
+        public int Threshold { get; } = 30;
+        public int Temperature 
+        { 
+            get => _temperature;
+            set
+            {
+                _temperature = value;
+
+                if (_temperature > Threshold)
+                {
+                    RaiseTempChangeEvent("Temperature is above threshold.");
+                }
+            }
+        }
+
+        protected virtual void RaiseTempChangeEvent(string msg)
+        {
+            OnTemperatureChange?.Invoke(msg);
+        }
+    }
+
+    public class TemperatureAlert
+    {
+        public void OnTemperatureChange(string msg)
+        {
+            Console.WriteLine($"Alert: {msg}");
+        }
+    }
+}
